4f09873698b12a60c14b202324e868958fdd81d5
Davy Assunção <davydesouzabar@gmail.com>
2023-05-02 21:40:37 -0400

    feat: create range mock

---
+++ src/pages/nutri-dashboard/mock-ranges.ts
@@ -0,0 +1,24 @@
+import { Range } from "react-date-range";
+
+export const mockRanges: Range[] = [
+  {
+    startDate: new Date(),
+    endDate: new Date(),
+  },
+  {
+    endDate: new Date("2023/05/10"),
+    startDate: new Date("2023/05/10"),
+  },
+  {
+    endDate: new Date("2023/05/15"),
+    startDate: new Date("2023/05/15"),
+  },
+  {
+    endDate: new Date("2023/06/10"),
+    startDate: new Date("2023/06/10"),
+  },
+  {
+    endDate: new Date("2023/07/11"),
+    startDate: new Date("2023/07/11"),
+  },
+];
