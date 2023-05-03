e1433853bf1696e6eca29bfac36ab02e87555c3b
Davy Assunção <davydesouzabar@gmail.com>
2023-05-02 21:37:40 -0400

    feat: create text component

---
+++ src/components/text/text.types.ts
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+import { TColors } from "../../global/colorsMapping";
+
+type TSize = "2" | "4" | "8" | "16" | "32" | "64";
+
+export interface IText {
+  size: TSize;
+  weight: number;
+  height: number;
+  color: TColors;
+  children: ReactNode;
+}
+
+export interface IStylesProps {
+  size: TSize;
+  weight: number;
+  height: number;
+  color: TColors;
+}
