36581e791bc1f93a1b4d94ddd7377ce8c38455fa
Davy Assunção <davydesouzabar@gmail.com>
2023-05-02 21:38:02 -0400

    feat: create colors mapping function

---
+++ src/global/colorsMapping.ts
@@ -0,0 +1,22 @@
+import { theme } from "./theme";
+
+export type TColors =
+  | "vinho"
+  | "amarelo"
+  | "laranja_claro"
+  | "laranja_medio"
+  | "laranja_escuro"
+  | "branco";
+
+export const colorsMapping = (color: TColors) => {
+  const colorsMap = {
+    vinho: theme.Colors.vinho,
+    amarelo: theme.Colors.amarelo,
+    laranja_claro: theme.Colors.laranja_claro,
+    laranja_medio: theme.Colors.laranja_medio,
+    laranja_escuro: theme.Colors.laranja_escuro,
+    branco: theme.Colors.branco,
+  };
+
+  return colorsMap[color];
+};
