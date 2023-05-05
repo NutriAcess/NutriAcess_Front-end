08cd986e587a975f57972f62dd5f3b7c5ed9ae68
unknown <lopes.claudia.csl@gmail.com>
2023-05-05 19:23:36 -0300

    feat: create page about

---
+++ src/home/sobre/sobre.tsx
@@ -0,0 +1,35 @@
+import { Container } from "./sobre.styles";
+import Info from "../components/info/info";
+import appleabout from "../../assets/appleabout.svg";
+import womenabout from "../../assets/womenabout.svg";
+
+const Sobre = () => {
+  return (
+    <Container>
+      <Info
+        title="Nutrição acessível, saúde imbatível!"
+        description="Fundada no primeiro semestre de 2023 por alunos do 
+            Instituto Proa, a NutriAcess é uma plataforma que visa 
+            a democratização a consultas nutricionais a preço social, 
+            comprometidos em oferecer um serviço acessível e de 
+            qualidade para todos os nossos pacientes."
+        direction="row"
+        image={{ alt: "", url: womenabout }}
+      />
+
+      <Info
+        title="Como a NutriAcess pode te ajudar?"
+        description="A NutriAcess pode te ajudar ao tornar o que é difícil 
+        parecer fácil, guiando você na busca pela 
+        sua melhor versão através da nossa plataforma. 
+        Contamos com profissionais qualificados, prontos para 
+        oferecer um atendimento personalizado, tudo a apenas 
+        um clique de distância."
+        direction="row-reverse"
+        image={{ alt: "", url: appleabout }}
+      />
+    </Container>
+  );
+};
+
+export default Sobre;
