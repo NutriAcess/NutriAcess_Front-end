bd4101816dcef5045c0fd2e390d4b75796b2dce7
unknown <lopes.claudia.csl@gmail.com>
2023-05-08 12:14:34 -0300

    feat: add new variant button

--- src/components/button/button.styles.ts
+++ src/components/button/button.styles.ts
@@ -1,18 +1,18 @@
 import styled from "styled-components";
 
 interface IStyles {
-    variant: "primario" | "secundario";
+    variant: "primario" | "secundario" | "terciario";
 }
 
 export const Container = styled.button<IStyles>`
     padding: 12px 24px;
     border-radius: 15px;
     background-color: ${({theme,variant}) => variant === "primario" ? theme.Colors.vinho : "transparent"};
-    border: 2px solid ${({theme,variant}) => variant === "secundario" ?  theme.Colors.vinho : theme.Colors.vinho};
+    border: 2px solid ${({theme,variant}) => variant === "secundario" ? theme.Colors.vinho : variant === "terciario" ? theme.Colors.laranja_destaque : "transparent"};
 `;
 
 export const TitleButton = styled.span<IStyles>`
     font-weight: bold;
     font-family: ${({theme}) => theme.Font.font_lato};
-    color: ${({theme,variant}) => variant === "primario" ? theme.Colors.branco : theme.Colors.vinho };
+    color: ${({theme,variant}) => variant === "primario" ? theme.Colors.branco : variant === "secundario" ? theme.Colors.vinho : theme.Colors.laranja_destaque };
 `;
