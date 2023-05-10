b7c99ef22fe35fdad6010f99b867003307b4df44
Luis Santana <luisroyale211@gmail.com>
2023-05-10 12:29:02 -0300

    feat: trade home word to agendamentos in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Home from './home/home';
+
 import { ThemeProvider } from 'styled-components';
 import { theme } from './global/theme';
+import Agendamentos from './agendamentos/agendamentos';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}><Home /></ThemeProvider>
+    <ThemeProvider theme={theme}><Agendamentos /></ThemeProvider>
   );
 }
 
