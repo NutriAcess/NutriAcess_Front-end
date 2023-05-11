d3389dab2c0b406a644fc24849d687e84268ddcf
unknown <lopes.claudia.csl@gmail.com>
2023-05-10 21:35:55 -0300

    feat: create Plans component

--- src/home/plans/plans.styles.ts
+++ src/home/plans/plans.styles.ts
@@ -2,14 +2,34 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     display: flex;
-    background-color: ${({theme}) => theme.Colors.amarelo};
-    height: 300px;
-    align-items: center;
     flex-direction: column;
 `;
 
-export const Title = styled.div`
+export const Header = styled.div`
+    display: flex;
+    height: 300px;
+    align-items: center;
+    flex-direction: column;
+    background-color: ${({theme}) => theme.Colors.amarelo};
+`;
+
+export const Plan = styled.span`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 30px;
+    margin-bottom: 50px;
+`;
+
+export const Title = styled.span`
     margin: 50px 0;
 `;
 
-export const SubTitle = styled.div``;
+export const SubTitle = styled.span``;
+
+export const CardPlanGlobal = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
--- src/home/plans/plans.tsx
+++ src/home/plans/plans.tsx
@@ -1,20 +1,134 @@
+import Button from "../../components/button/button";
 import { Text } from "../../components/text/text";
-import { Container, SubTitle, Title } from "./plans.styles";
+import CardPlan from "../components/cardPlan/cardPlan";
+import {
+  CardPlanGlobal,
+  Container,
+  Header,
+  Plan,
+  SubTitle,
+  Title,
+} from "./plans.styles";
+
+const TopicsPlus = [
+  {
+    isChecked: true,
+    title: "1 consulta por mês",
+  },
+  {
+    isChecked: true,
+    title: "Suporte online",
+  },
+  {
+    isChecked: false,
+    title: "Notificação - Lembrete",
+  },
+  {
+    isChecked: false,
+    title: "Acompanhamento pessoal",
+  },
+];
+
+const TopicsFamilia = [
+  {
+    isChecked: true,
+    title: "1 consulta por mês",
+  },
+  {
+    isChecked: true,
+    title: "Suporte online",
+  },
+  {
+    isChecked: true,
+    title: "Notificação - Lembrete",
+  },
+  {
+    isChecked: true,
+    title: "Acompanhamento pessoal",
+  },
+];
+
+const TopicsPremium = [
+  {
+    isChecked: true,
+    title: "1 consulta por mês",
+  },
+  {
+    isChecked: true,
+    title: "Suporte online",
+  },
+  {
+    isChecked: true,
+    title: "Notificação - Lembrete",
+  },
+  {
+    isChecked: true,
+    title: "Acompanhamento pessoal",
+  },
+];
 
 const Plans = () => {
   return (
     <Container>
-      <Title>
-        <Text weight={600} height={1.7} color="branco" size="32">
-          Conheça nossos planos
-        </Text>
-      </Title>
-      <SubTitle>
-        <Text weight={400} height={1.7} color="branco" size="24">
-          Experimente nossas soluções e escolha o plano ideal para o seu próximo
-          projeto.
-        </Text>
-      </SubTitle>
+      <Header>
+        <Title>
+          <Text weight={600} height={1.7} color="branco" size="32">
+            Conheça nossos planos
+          </Text>
+        </Title>
+        <SubTitle>
+          <Text weight={400} height={1.7} color="branco" size="16">
+            Experimente nossas soluções e escolha o plano ideal para o seu
+            próximo projeto.
+          </Text>
+        </SubTitle>
+      </Header>
+      <Plan>
+        <CardPlanGlobal>
+          <CardPlan
+            destaque="secundario"
+            shadow="secundario"
+            titleplan="Plus"
+            description="Venha conhecer as vantagens do plano Plus!"
+            plan={{
+              value: 50,
+              duration: "por mês",
+            }}
+            topics={TopicsPlus}
+          />
+          <Button variant="primario" title="Assinar"></Button>
+        </CardPlanGlobal>
+
+        <CardPlanGlobal>
+          <CardPlan
+            destaque="primario"
+            shadow="primario"
+            titleplan="Plano Família"
+            description="Venha conhecer as vantagens do plano Família!"
+            plan={{
+              value: 120,
+              duration: "por mês",
+            }}
+            topics={TopicsFamilia}
+          />
+          <Button variant="terciario" title="Assinar"></Button>
+        </CardPlanGlobal>
+
+        <CardPlanGlobal>
+          <CardPlan
+            destaque="secundario"
+            shadow="secundario"
+            titleplan="Plano Premium"
+            description="Venha conhecer as vantagens do plano Premium!"
+            plan={{
+              value: 100,
+              duration: "por 3 meses",
+            }}
+            topics={TopicsPremium}
+          />
+          <Button variant="primario" title="Assinar"></Button>
+        </CardPlanGlobal>
+      </Plan>
     </Container>
   );
 };
