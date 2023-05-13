072407d3c34a752700b4422f8c663e470cb8b5f9
unknown <lopes.claudia.csl@gmail.com>
2023-05-13 17:20:01 -0300

    feat: add props for component button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,12 +3,23 @@ import { Container, TitleButton } from "./button.styles";
 interface IButton {
   title: string;
   variant: "primario" | "secundario" | "terciario";
+  width: number;
+  padding: number;
+  border: number;
+  size: number;
 }
 
-const Button = ({ title, variant }: IButton) => {
+const Button = ({ title, variant, width, padding, border, size }: IButton) => {
   return (
-    <Container variant={variant}>
-      <TitleButton variant={variant}>{title}</TitleButton>
+    <Container
+      variant={variant}
+      width={width}
+      padding={padding}
+      border={border}
+    >
+      <TitleButton variant={variant} size={size}>
+        {title}
+      </TitleButton>
     </Container>
   );
 };
