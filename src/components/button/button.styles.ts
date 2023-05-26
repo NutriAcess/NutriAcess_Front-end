ce3956d93b9b3935881d71bc3918faccce20afc1
Davy Assunção <davydesouzabar@gmail.com>
2023-05-26 08:13:43 -0400

    feat(nutri-dash): resolve merge conflicts

--- src/components/button/button.styles.ts
+++ src/components/button/button.styles.ts
@@ -1,49 +1,5 @@
-import styled, { DefaultTheme } from "styled-components";
+import styled from "styled-components";
 
-<<<<<<< HEAD
-type TVariant = "primario" | "secundario" | "terciario";
-interface IStyles {
-  variant: TVariant;
-}
-
-function getColorVariant(variant: TVariant, theme: DefaultTheme) {
-  switch (variant) {
-    case "primario":
-      return theme.Colors.branco;
-    case "secundario":
-      return theme.Colors.vinho;
-    default:
-      return theme.Colors.preto;
-  }
-}
-
-function getBackgroundVariant(variant: TVariant, theme: DefaultTheme) {
-  switch (variant) {
-    case "primario":
-      return theme.Colors.vinho;
-    case "secundario":
-      return "transparent";
-    default:
-      return theme.Colors.amarelo;
-  }
-}
-
-export const Container = styled.button<IStyles>`
-  padding: 12px 24px;
-  border-radius: 15px;
-  background-color: ${({ theme, variant }) =>
-    getBackgroundVariant(variant, theme)};
-
-  border: 2px solid
-    ${({ theme, variant }) =>
-      variant === "secundario" ? theme.Colors.vinho : "transparent"};
-`;
-
-export const TitleButton = styled.span<IStyles>`
-  font-weight: bold;
-  font-family: ${({ theme }) => theme.Font.font_lato};
-  color: ${({ theme, variant }) => getColorVariant(variant, theme)};
-=======
 interface IContainer {
   variant: "primario" | "secundario" | "terciario";
 }
@@ -72,5 +28,4 @@ export const TitleButton = styled.span<IContainer>`
       : variant === "secundario"
       ? theme.Colors.vinho
       : theme.Colors.laranja_destaque};
->>>>>>> origin/home
 `;
