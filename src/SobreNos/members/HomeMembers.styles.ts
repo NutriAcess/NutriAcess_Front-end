f0968765c277831d46359507913da380466dd269
Luis Santana <luisroyale211@gmail.com>
2023-05-27 22:29:40 -0300

    feat: create interface and style

---
+++ src/SobreNos/members/HomeMembers.styles.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+display: flex;
+  flex-direction: column;
+justify-content: center;
+`;
+
+export const ImageMember = styled.div`
+
+  img{
+  border: 10px solid #ed6f0d;
+  border-radius: 50%;
+  }
+`;
+
+export const Title = styled.div`
+  font-size: 1.3rem;
+  font-weight: 700;
+`;
+
+export const Stack = styled.div`
+`;
+
+export const SocialMedia = styled.div`
+`;
+
+export const Information = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  
+`;
+
+
+
+
