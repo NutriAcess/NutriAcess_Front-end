745ac38a20289748145136db6e1bd4f64fc220a3
Luis Santana <luisroyale211@gmail.com>
2023-05-28 19:02:44 -0300

    commit rapida

--- src/SobreNos/AppSobreNos.styles.ts
+++ src/SobreNos/AppSobreNos.styles.ts
@@ -5,8 +5,11 @@ export const Container = styled.div`
 
 export const BigTitle = styled.div`
   display: grid;
+
   font-size: 3rem;
+
   justify-content: center;
+
   margin-top: 20px;
   margin-bottom: 10px;
 `;
@@ -15,9 +18,12 @@ export const Content = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+
   gap: 90px;
+
   align-items: center;
   justify-content: center;
+
   margin-top: 30px;
 `;
 
@@ -25,9 +31,12 @@ export const Contenter = styled.div `
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+
   gap: 90px;
+
   align-items: center;
   justify-content: center;
+  
   margin-top: 30px;
   margin-bottom: 30px;
 `;
