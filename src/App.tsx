0115eb147143de500e1a43850bf05a513a378638
Davy Assunção <davydesouzabar@gmail.com>
2023-05-28 17:38:04 -0400

    feat: create navigation routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "./global/theme";
-import Singin from "./pages/signInEsp/signInEsp";
+import { AppRoutes } from "./routes/routes";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Singin />
+      <AppRoutes />
     </ThemeProvider>
   );
 }
---
+++ src/routes/routes.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../pages/home/home";
+import SignInUser from "../pages/signInUser/signInUser";
+import SignInSpecialist from "../pages/signInEsp/signInEsp";
+import { NutriDashboard } from "../pages/nutri-dashboard/nutri-dasboard";
+import SignUpUser from "../pages/signUpUser/signUpUser";
+
+export const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in-user" element={<SignInUser />} />
+        <Route path="/sign-in-esp" element={<SignInSpecialist />} />
+        <Route path="/sign-up-user" element={<SignUpUser />} />
+        <Route path="/dashboard-nutri" element={<NutriDashboard />} />
+      </Routes>
+    </Router>
+  );
+};
