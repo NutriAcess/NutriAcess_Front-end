98a6f5353df79b620ceef77323a2f2de3ea38f9c
Davy Assunção <davydesouzabar@gmail.com>
2023-05-28 15:08:55 -0400

    feat(schedulignCard): create schedulignCard component

--- src/pages/nutri-dashboard/components/schedulignCard/schedulingCard.tsx
+++ src/pages/nutri-dashboard/components/schedulignCard/schedulingCard.tsx
@@ -1,3 +1,39 @@
+import { VideoCamera } from "@phosphor-icons/react";
+import Button from "../../../../components/button/button";
+import { Text } from "../../../../components/text/text";
+import { Container, PatientInfo } from "./schedulingCard.styles";
+import { useTheme } from "styled-components";
+
 export const SchedulingCard = () => {
-  return <div />;
+  const theme = useTheme();
+
+  return (
+    <Container>
+      <PatientInfo>
+        <div>
+          <Text color="preto" height={26} size="22" weight={500}>
+            Data e hora: 31/10/2024 - 10:34 hrs
+          </Text>
+        </div>
+        <div>
+          <Text color="preto" height={26} size="22" weight={500}>
+            Paciente: Jonh Doe
+          </Text>
+        </div>
+
+        <div>
+          <Text color="preto" height={26} size="22" weight={500}>
+            Descrição: Lorem ipsum...
+          </Text>
+        </div>
+      </PatientInfo>
+      <Button
+        icon={
+          <VideoCamera color={theme.Colors.branco} weight="regular" size={24} />
+        }
+        title="Entrar na chamada"
+        variant="primario"
+      />
+    </Container>
+  );
 };
