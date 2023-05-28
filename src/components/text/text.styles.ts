56501b062f527d49a3a55bca3bc8480ffe4cb249
unknown <lopes.claudia.csl@gmail.com>
2023-05-27 22:01:16 -0300

    fix: return px in line-height

--- src/components/text/text.styles.ts
+++ src/components/text/text.styles.ts
@@ -1,11 +1,11 @@
 import styled, { css } from "styled-components";
-import { IStylesProps } from "./text.types";
 import { colorsMapping } from "../../global/colorsMapping";
+import { IStylesProps } from "./text.types";
 
 export const CustomSpan = styled.span<IStylesProps>`
   color: ${({ color }) => colorsMapping(color)};
   font-weight: ${({ weight }) => weight};
-  line-height: ${({ height }) => height};
+  line-height: ${({ height }) => height}px;
   font-size: ${({ size }) => size}px;
   font-family: ${({ theme }) => theme.Font.font_lato};
 
