4c100eae5017a6f8432ac44ed32e12b63fa50eaa
Davy Assunção <davydesouzabar@gmail.com>
2023-05-28 17:07:04 -0400

    feat: create esp signIn

--- src/pages/signInEsp/signInEsp.styles.ts
+++ src/pages/signInEsp/signInEsp.styles.ts
@@ -1,22 +1,31 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    display: flex;
-    margin-top:25px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  flex-direction: column;
+
+  height: 100%;
 `;
 
-export const Texto1 = styled.div`
-    position: absolute;
-    left: 42%;
-    top: 15%;
-    color: #741944;
+export const Form = styled.div`
+  margin-top: 36px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  flex-direction: column;
+
+  gap: 24px;
 `;
 
-export const Texto2 = styled.div`
-    position: absolute;
-    left: 40%;
-    top: 70%;
-    color: #741944;
+export const InputWrapper = styled.div`
+  width: 600px;
 `;
 
-
+export const ButtonWrapper = styled.div`
+  width: 200px;
+`;
