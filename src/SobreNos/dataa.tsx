b18e6bc1b47895a1afebe985adec45689a9b8273
Luis Santana <luisroyale211@gmail.com>
2023-05-29 16:33:22 -0300

    feat: create second  map method

---
+++ src/SobreNos/dataa.tsx
@@ -0,0 +1,92 @@
+import FotoClaudia from "../assets/Claudia.svg"
+import FotoSamuel from "../assets/Samuca.svg"
+import FotoHenrique from "../assets/Henrique.svg"
+import FotoLais from "../assets/Lais.svg"
+import FotoLilian from "../assets/Lilian.svg"
+
+
+import LogoLinkedin from "../assets/linkedin.svg"
+import LogoGithub from "../assets/github.svg"
+export const dataa = [
+
+{image: {alt: "", url: FotoClaudia},
+      
+      
+  title:"Claudia Lopes",
+
+  stack:"Front-end",
+
+  hrefpessoalhub:"https://github.com/ClahLopes",
+
+  hrefpessoallink:"https://www.linkedin.com/in/claudiadossantoslopes/",
+
+  ancor_linktree:"https://linktr.ee/ClahLopes",
+  
+
+  social_media:{alt: "", url: LogoLinkedin},
+  social_mediaa:{alt: "", url: LogoGithub}},
+
+  {image: {alt: "", url: FotoSamuel},
+      
+      
+  title:"Samuel Allan",
+
+  stack:"Front-end",
+
+  hrefpessoalhub:"https://github.com/SAMUKISZHSD",
+
+  hrefpessoallink:"https://www.linkedin.com/in/samuel-allan-635290258/",
+
+  ancor_linktree:"https://linktr.ee/Samuelallannunes",
+
+  social_media:{alt: "", url: LogoLinkedin},
+  social_mediaa:{alt: "", url: LogoGithub}},
+
+  {image: {alt: "", url: FotoHenrique},
+      
+      
+  title:"Henrique Araújo",
+
+  stack:"Back-end",
+
+  hrefpessoalhub:"https://github.com/Henriquesantos12",
+
+  hrefpessoallink:"https://www.linkedin.com/in/henrique--dev/",
+
+  ancor_linktree:"https://linktr.ee/_HenriqueSantos?utm_source=linktree_profile_share&ltsid=f9d2d37f-a148-4507-8d8c-aa7b9371d4ee",
+
+  social_media:{alt: "", url: LogoLinkedin},
+  social_mediaa:{alt: "", url: LogoGithub}},
+
+  {image: {alt: "", url: FotoLais},
+      
+      
+  title:"Lais Carvalho",
+
+  stack:"Front-end",
+
+  hrefpessoalhub:"https://github.com/Lais205",
+
+  hrefpessoallink:"https://www.linkedin.com/in/lais-carvalho-772113268/",
+
+  ancor_linktree:"https://linktr.ee/lais_205",
+
+  social_media:{alt: "", url: LogoLinkedin},
+  social_mediaa:{alt: "", url: LogoGithub}},
+
+  {image: {alt: "", url: FotoLilian},
+      
+      
+  title:"Lilian Carvalho",
+
+  stack:"Front-end",
+
+  hrefpessoalhub:"https://github.com/Lais205",
+
+  hrefpessoallink:"https://github.com/Lilian-Carvalho25",
+
+  ancor_linktree:"https://linktr.ee/lilian.csribeiro",
+
+  social_media:{alt: "", url: LogoLinkedin},
+  social_mediaa:{alt: "", url: LogoGithub}},
+]
