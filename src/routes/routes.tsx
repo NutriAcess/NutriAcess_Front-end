6a5fe319a5b3eaeec46ee0cdb1beafc8c000e8f2
unknown <lopes.claudia.csl@gmail.com>
2023-05-29 20:02:04 -0300

    feat: create marcar-consulta route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,8 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { MarcarConsulta } from "../marcarConsulta/marcarConsulta";
 import Home from "../pages/home/home";
-import SignInUser from "../pages/signInUser/signInUser";
-import SignInSpecialist from "../pages/signInEsp/signInEsp";
 import { NutriDashboard } from "../pages/nutri-dashboard/nutri-dasboard";
+import SignInSpecialist from "../pages/signInEsp/signInEsp";
+import SignInUser from "../pages/signInUser/signInUser";
 import SignUpUser from "../pages/signUpUser/signUpUser";
 
 export const AppRoutes = () => {
@@ -14,6 +15,7 @@ export const AppRoutes = () => {
         <Route path="/sign-in-esp" element={<SignInSpecialist />} />
         <Route path="/sign-up-user" element={<SignUpUser />} />
         <Route path="/dashboard-nutri" element={<NutriDashboard />} />
+        <Route path="/marcar-consulta" element={<MarcarConsulta />} />
       </Routes>
     </Router>
   );
