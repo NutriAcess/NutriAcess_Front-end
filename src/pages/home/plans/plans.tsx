72db48b324f9116391d8bf292cde0b2fb00d818d
unknown <lopes.claudia.csl@gmail.com>
2023-05-29 19:56:48 -0300

    feat: resolve conflict plans

--- src/pages/home/plans/plans.tsx
+++ src/pages/home/plans/plans.tsx
@@ -1,31 +1,14 @@
-import { Text } from "../../../components/text/text";
 import CardPlan from "../components/cardPlan/cardPlan";
 import { TopicsFamilia, TopicsPlus, TopicsPremium } from "./data";
 import {
   CardPlanGlobal,
   Container,
-  Header,
   Plan,
-  SubTitle,
-  Title,
 } from "./plans.styles";
 
 const Plans = () => {
   return (
     <Container>
-      <Header>
-        <Title>
-          <Text weight={700} height={48} color="branco" size="32">
-            Conheça nossos planos
-          </Text>
-        </Title>
-        <SubTitle>
-          <Text weight={400} height={22} color="branco" size="18">
-            Experimente nossas soluções e escolha o plano ideal para o seu
-            próximo projeto.
-          </Text>
-        </SubTitle>
-      </Header>
       <Plan>
         <CardPlanGlobal shadow="secundario">
           <CardPlan
