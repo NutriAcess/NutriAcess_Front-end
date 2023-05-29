bb285eac158f4312d7dd52519c02ca9a1c2abcd8
unknown <lopes.claudia.csl@gmail.com>
2023-05-28 21:49:17 -0300

    feat: create component SchedulingCard

---
+++ src/pages/profile-user/components/schedulignCard/schedulingCard.styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 32px 24px;
+
+  border: 1px solid ${({ theme }) => theme.Colors.laranja_destaque};
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 8px;
+
+  background-color: ${({ theme }) => theme.Colors.branco};
+
+  margin-inline: 200px;
+`;
+
+export const PatientInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
