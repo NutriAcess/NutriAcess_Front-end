ce328e8c85d2f79bc7537ba32bf597532aba15df
Samuel Allan Nunes da Cunha <117799926+SAMUKISZHSD@users.noreply.github.com>
2023-05-29 16:23:54 -0300

    style: estilo pagina cadastro especialista

---
+++ src/pages/signUpEsp/signUpEsp.styles.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  flex-direction: column;
+  
+  height: 100%;
+`;
+
+export const Form = styled.div`
+  margin-top: 36px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  flex-direction: column;
+
+  gap: 8px;
+`;
+
+export const InputWrapper = styled.div`
+  width: 600px;
+`;
+
+export const ButtonWrapper = styled.div`
+ margin-top: 15px;
+ margin-bottom: 5px;
+  width: 200px;
+`;
