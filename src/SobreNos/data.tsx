0d0a7e0ae3b31230533ea1c6d3d4090c3ebe95b4
Luis Santana <luisroyale211@gmail.com>
2023-05-29 16:33:02 -0300

    feat: create first map method

---
+++ src/SobreNos/data.tsx
@@ -0,0 +1,99 @@
+import LogoLinkedin from "../assets/linkedin.svg"
+import LogoGithub from "../assets/github.svg"
+import LogoLinktree from "../assets/linktree.svg"
+
+import LogoNutriAcess from "../assets/logonutriacess.svg"
+import FotoLuis from "../assets/luis.svg"
+import FotoAlan from "../assets/Alan.svg"
+import FotoGusta from "../assets/Gustavo.svg"
+import FotoErica from "../assets/Erica.svg"
+
+
+
+export const data = [
+  {image: {alt: "", url: FotoAlan},
+      
+      
+  title:"Alan Gonçalves",
+
+  stack:"Full Stack",
+
+  hrefpessoalhub:"https://github.com/lanDev01",
+
+  hrefpessoallink:"https://www.linkedin.com/in/alangon%C3%A7alvesjr/",
+
+  ancor_linktree:"https://www.linkedin.com/in/alan-gon%C3%A7alves-261699244/",
+
+
+  social_media:{alt: "", url: LogoLinkedin},
+  social_mediaa:{alt: "", url: LogoGithub}},
+
+  {image: {alt: "", url: FotoLuis},
+      
+      
+  title:"Luis Santana",
+
+  stack:"Full Stack",
+
+  hrefpessoalhub:"https://github.com/Luisjunior119",
+
+  hrefpessoallink:"https://www.linkedin.com/in/luis-santana-junior/",
+
+  ancor_linktree:"https://linktr.ee/Luis_Santana",
+
+
+  social_media:{alt: "", url: LogoLinkedin},
+  social_mediaa:{alt: "", url: LogoGithub}},
+
+  {image: {alt: "", url: LogoNutriAcess},
+      
+      
+  title:"Contato NutriAcess",
+
+  stack:"Linktree e Github",
+
+  hrefpessoalhub:"https://github.com/Luisjunior119/demoday-nutriacess",
+
+  hrefpessoallink:"https://linktr.ee/nutriacess_",
+
+  ancor_linktree:"https://linktr.ee/nutriacess_",
+
+
+  social_media:{alt: "", url: LogoLinktree},
+  social_mediaa:{alt: "", url: LogoGithub}},
+
+  {image: {alt: "", url: FotoGusta},
+      
+      
+  title:"Gustavo Senise",
+
+  stack:"Back-end",
+
+  hrefpessoalhub:"https://github.com/gussenise",
+
+  hrefpessoallink:"https://www.linkedin.com/in/gustavosenise/",
+
+  ancor_linktree:"https://linktr.ee/gusenise",
+
+
+  social_media:{alt: "", url: LogoLinkedin},
+  social_mediaa:{alt: "", url: LogoGithub}},
+
+  {image: {alt: "", url: FotoErica},
+      
+      
+  title:"Erica Inacia",
+
+  stack:"Back-end",
+
+  hrefpessoalhub:"https://github.com/EricaInaciadeLima",
+
+  hrefpessoallink:"https://www.linkedin.com/in/erica-inacia-82b7b7206/",
+
+  ancor_linktree:"https://linktr.ee/Ericainacia27",
+
+  social_media:{alt: "", url: LogoLinkedin},
+  social_mediaa:{alt: "", url: LogoGithub}},
+
+  
+]
