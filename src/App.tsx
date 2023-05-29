f81e1cfa4717ab683298bff65945202de4dbddc1
unknown <lopes.claudia.csl@gmail.com>
2023-05-28 21:51:32 -0300

    feat: change SignIn To ProfileUser

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "./global/theme";
-import Singin from "./pages/signInEsp/signInEsp";
+import { ProfileUser } from "./pages/profile-user/profile-user";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Singin />
+      <ProfileUser />
     </ThemeProvider>
   );
 }
