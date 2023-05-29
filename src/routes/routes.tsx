dc8b46007963e9690da367800d6ac2f3fc061567
Samuel Allan Nunes da Cunha <117799926+SAMUKISZHSD@users.noreply.github.com>
2023-05-29 16:25:39 -0300

    feat: create route for sign up especialista

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,12 +4,14 @@ import SignInUser from "../pages/signInUser/signInUser";
 import SignInSpecialist from "../pages/signInEsp/signInEsp";
 import { NutriDashboard } from "../pages/nutri-dashboard/nutri-dasboard";
 import SignUpUser from "../pages/signUpUser/signUpUser";
+import SignUpEsp from "../pages/signUpEsp/signUpEsp";
 
 export const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home/>} />
+        <Route path="/sign-up-esp" element={<SignUpEsp />} />
         <Route path="/sign-in-user" element={<SignInUser />} />
         <Route path="/sign-in-esp" element={<SignInSpecialist />} />
         <Route path="/sign-up-user" element={<SignUpUser />} />
