c44b95c3b30d38b6cd1ad5f2fdc74838ef44e05b
Luis Santana <luisroyale211@gmail.com>
2023-05-28 21:30:01 -0300

    feat: add agendamentos to route file

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,14 +4,17 @@ import SignInUser from "../pages/signInUser/signInUser";
 import SignInSpecialist from "../pages/signInEsp/signInEsp";
 import { NutriDashboard } from "../pages/nutri-dashboard/nutri-dasboard";
 import SignUpUser from "../pages/signUpUser/signUpUser";
+import Agendamentos from "../agendamentos/agendamentos";
 
 export const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/agendamentos" element={<Agendamentos />} />
         <Route path="/sign-in-user" element={<SignInUser />} />
-        <Route path="/sign-in-esp" element={<SignInSpecialist />} />
+        <Route path="/sign-in-esp" element={<SignInSpecialist />}
+         />
         <Route path="/sign-up-user" element={<SignUpUser />} />
         <Route path="/dashboard-nutri" element={<NutriDashboard />} />
       </Routes>
