db86dc43cb9bfbc9da91575d1cd1126d9935bd0a
unknown <lopes.claudia.csl@gmail.com>
2023-05-29 22:15:29 -0300

    feat: component mock-schedulingPlan

---
+++ src/pages/profile-user/components/schedulignPlan/mock-schedulingPlan.ts
@@ -0,0 +1,30 @@
+export const mocksSchedulingPlan = [
+    {
+        name : "Marcos",
+        age: "30 anos",
+        sex: "Masculino",
+        height: "1,70",
+        weight: "70kg",
+    },
+    {
+        name : "Aline",
+        age: "18 anos",
+        sex: "Feminino",
+        height: "1,60",
+        weight: "45kg",
+    },
+    {
+        name : "Marcos",
+        age: "30 anos",
+        sex: "Masculino",
+        height: "1,70",
+        weight: "70kg",
+    },
+    {
+        name : "Marcos",
+        age: "30 anos",
+        sex: "Masculino",
+        height: "1,70",
+        weight: "70kg",
+    }
+]
