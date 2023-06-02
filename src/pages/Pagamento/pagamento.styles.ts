04fd8bb3da248c82a88b5aff7ce70b30d92f3417
Samuel Allan Nunes da Cunha <117799926+SAMUKISZHSD@users.noreply.github.com>
2023-06-02 13:13:56 -0300

    style: create area de pagamento

---
+++ src/pages/Pagamento/pagamento.styles.ts
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+
+
+
+export const Container = styled.div`
+ 
+`;
+
+export const Cont = styled.div`
+  
+   
+  margin-left: 1px;
+`;
+
+export const VerticalBar = styled.div`
+  width: 2px;
+  display: flex;
+  margin-left: 38%;
+  margin-top: -10%;
+  height: 600px;
+  background-color: black;
+`;
+
+
+
+export const Inputstyle = styled.div`
+  width: 600px;
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+
+
+export const Textstyle1 = styled.div`
+  display: flex;
+  margin-top: 8%;
+ margin-left: 2%;
+  
+`;
+
+export const Textstyle2 = styled.div`
+  display: flex;
+  margin-left: 2%;
+  
+`;
+
+export const Form = styled.div`
+  margin-left: 20px;
+  display: flex;
+  flex-direction: column;
+
+`;
+
+export const Inputradio = styled.div`
+ display: flex;
+ margin-left: 2%;
+ margin-top: 1%;
+ font-size: 20px;
+ 
+
+  
+
+`;
+
+
+export const Inputstyle1 = styled.div`
+  width: 250px;
+  display: flex;
+  
+`;
+
+
+export const Inputstyle2 = styled.div`
+  width: 250px;
+  display: flex;
+  margin-top: -3%;
+  margin-left: 20%;
+  
+`;
+
+ export const StyledCheckbox = styled.div`
+   display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  text-align: justify;
+  margin-left:30px;
+
+ `;
+
+
+export const Nutristyle = styled.div`
+  
+  
+`;
