93d5fd7ee6f8288cd2510b327b9518e050ff06a7
Claudia dos Santos Lopes <lopes.claudia.csl@gmail.com>
2023-06-07 17:57:37 -0300

    fix: call created components

--- src/pages/Pagamento/pagamento.styles.ts
+++ src/pages/Pagamento/pagamento.styles.ts
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    
 `;
 
 export const Content = styled.div`
@@ -9,57 +8,23 @@ export const Content = styled.div`
   margin-top: 100px;
 `;
 
-export const SideLeft = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 45%;
-  gap: 10px;
-  padding-left: 50px;
-  border-right: 1px solid ${({theme}) => theme.Colors.vinho};
-`;
-
 export const SideRight = styled.div`
   width:50%;
   margin-left: 50px;
 `;
 
-export const VerticalBar = styled.div`
-  width: 2px;
-  display: flex;
-  margin-left: 38%;
-  margin-top: -10%;
-  height: 600px;
-  background-color: black;
-`;
-
-export const ContentInput = styled.div`
+export const HeaderPayment = styled.div`
     display: flex;
-    width: 80%;
     justify-content: space-between;
-    gap: 20px;
 `;
 
-export const Inputstyle = styled.div`
-  width: 80%;
-  display: flex;
-  margin-bottom: 20px;
-`;
 
-export const StyledCheckbox = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: flex-start;
-    text-align: justify;
- `;
 
-export const SectionChekbox = styled.div`
-    margin-left: 2%;
-    margin-top: 25px;
-`;
+
 
 export const ButtonConfirm = styled.div`
     display: flex;
-    width: 70%;
-    justify-content: end;
+    margin: 10px 30px;
+    justify-content: center;
     align-items: center;
 `;
--- src/pages/Pagamento/pagamento.tsx
+++ src/pages/Pagamento/pagamento.tsx
@@ -1,101 +1,32 @@
 import Header from '../../components/header/header';
-import { Input } from "../../components/input/input";
-import { Text } from '../../components/text/text';
-import Checkbox from './componentes/inputcheack';
 
-import React, { useState } from "react";
 
 import Button from '../../components/button/button';
 import { InfoNutri } from './componentes/infonutri/infonutri';
 import { ResumePag } from './componentes/resumepag/resumepag';
+import { SideLeft } from './componentes/sideLeft/sideLeft';
+import Typeplan from './componentes/typeplan/typeplan';
 import {
   ButtonConfirm,
   Container,
   Content,
-  ContentInput,
-  Inputstyle,
-  SectionChekbox,
-  SideLeft,
-  SideRight,
-  StyledCheckbox
+  HeaderPayment,
+  SideRight
 } from "./pagamento.styles";
 
 const Formasdepagamento = () => {
-  const [isChecked, setIsChecked] = useState(false);
-  const [isChecked1, setIsChecked1] = useState(false);
-  const [isChecked2, setIsChecked2] = useState(false);
-
-  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIsChecked(e.target.checked);
-  };
-
-  const handleCheckbox1Change = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIsChecked1(e.target.checked);
-  };
-
-  const handleCheckbox2Change = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIsChecked2(e.target.checked);
-  };
-
-  const [nometitular, setNometitular] = useState("");
-  const [ncartao, setNcartao] = useState("");
-  const [validade, setValidade] = useState("");
-  const [cods, setCods] = useState("");
-
-  const handleNometitularChange = (value: string) => {
-    setNometitular(value);
-  };
-
-  const handleNcartaoChange = (value: string) => {
-    setNcartao(value);
-  };
-
-  const handleValidadeChange = (value: string) => {
-    setValidade(value);
-  };
-
-  const handleCodsChange = (value: string) => {
-    setCods(value);
-  };
 
   return (
     <Container>
       <Header />
       <Content>
-        <SideLeft>
-          <Text color='preto' size='24' height={50} weight={600}>Método de Pagamento</Text>
-
-          <Text color='preto' size='20' height={31} weight={600}>Cartão de Crédito</Text>
-
-          <Inputstyle>
-            <Input label='' onChange={handleNometitularChange} placeholder='Nome do titular do cartão' value={nometitular} type='text'></Input>
-          </Inputstyle>
-          <Inputstyle>
-            <Input label='' onChange={handleNcartaoChange} placeholder='Número do cartão ' value={ncartao} ></Input>
-          </Inputstyle>
-
-          <ContentInput>
-            <Inputstyle>
-              <Input label='' onChange={handleValidadeChange} placeholder='Validade' value={validade} />
-            </Inputstyle>
-            <Inputstyle>
-              <Input label='' onChange={handleCodsChange} placeholder='Cód. de segurança' value={cods} />
-            </Inputstyle>
-          </ContentInput>
-
-
-          <SectionChekbox>
-            <Text color='preto' size='20' height={50} weight={200}>Qual plano você deseja?</Text>
-            <StyledCheckbox>
-              <Checkbox name="myCheckbox" checked={isChecked} onChange={handleCheckboxChange} text=' Plano Plus' />
-              <Checkbox name="myCheckbox" checked={isChecked1} onChange={handleCheckbox1Change} text=' Plano Premium' />
-              <Checkbox name="myCheckbox" checked={isChecked2} onChange={handleCheckbox2Change} text=' Plano Família' />
-            </StyledCheckbox>
-          </SectionChekbox>
-        </SideLeft>
-
+        <SideLeft/>
+          
         <SideRight>
-          <InfoNutri />
+          <HeaderPayment>
+            <InfoNutri />
+            <Typeplan/>
+          </HeaderPayment>
           <ResumePag/>
           <ButtonConfirm>
             <Button title='Confirmar pagamento' variant='primario' xs />
