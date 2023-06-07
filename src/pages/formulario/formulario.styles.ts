f2cdc2a4a71a32e4e02d7a0cb25afebe28a22c81
Claudia dos Santos Lopes <lopes.claudia.csl@gmail.com>
2023-06-07 11:40:56 -0300

    feat: created component formulario

--- src/pages/formulario/formulario.styles.ts
+++ src/pages/formulario/formulario.styles.ts
@@ -1,7 +1,23 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+`;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+    display: flex;
+    margin-top: 50px;
+    margin-bottom: 20px;
+`;
 
-export const StapOne = styled.div``;
+export const Box = styled.div`
+    width: 100%;
+`;
+
+export const ButtonStep = styled.div`
+    display: flex;
+    margin-top: 15px;
+    gap:10px;
+`;
--- src/pages/formulario/formulario.tsx
+++ src/pages/formulario/formulario.tsx
@@ -1,12 +1,52 @@
-import stapByStap from "../../assets/stapbystap.svg"
-import { FirstStap } from "./firstStap/firstStap"
-import { Container } from "./formulario.styles"
+import { useState } from "react";
+import Button from "../../components/button/button";
+import { FirstStap } from "./firstStap/firstStap";
+import { Box, ButtonStep, Container, Content } from "./formulario.styles";
+import { SecondStap } from "./secondStap/secondStap";
+import Step from "./step/step";
+import { ThirdStap } from "./thirdStap/thirdStap";
 
 export const Formulario = () => {
+  const [step, setStep] = useState(1);
+  const Steps = [1, 2, 3];
+
+  const getCompStep = () => {
+    switch (step) {
+      case 1:
+        return <FirstStap />;
+      case 2:
+        return <SecondStap />;
+      case 3:
+        return <ThirdStap />;
+      default:
+        return <FirstStap />;
+    }
+  };
+
   return (
     <Container>
-        <img src={stapByStap} alt="Passo-a-passo"/>
-        <FirstStap/>
+      <Content>
+        {Steps.map((item) => (
+          <Step key={item} index={item} active={step === item} />
+        ))}
+      </Content>
+      
+      <Box>
+      {getCompStep()}
+      </Box>
+      <ButtonStep>
+        <Button
+          title="Voltar"
+          variant="primario"
+          disable={step === 1}
+          onClick={() => setStep(step - 1)}
+        />
+        <Button
+          title={step === 3 ? "Enviar" : "Próximo"}
+          variant="primario"
+          onClick={() => step !== 3 && setStep(step + 1)}
+        />
+      </ButtonStep>
     </Container>
   )
 }
