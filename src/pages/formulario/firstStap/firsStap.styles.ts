1965245e4366aaf4447f7e2335d19f8876cbfb0a
Claudia dos Santos Lopes <lopes.claudia.csl@gmail.com>
2023-06-07 11:41:10 -0300

    feat: created component firstStap

--- src/pages/formulario/firstStap/firsStap.styles.ts
+++ src/pages/formulario/firstStap/firsStap.styles.ts
@@ -1,6 +1,37 @@
 import styled from "styled-components";
 
 
-export const Container = styled.div``
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
 
-export const Content = styled.div``
+export const TitleStap = styled.div`
+    margin: 10px 0px;
+`;
+
+export const SectionGlobalInputs = styled.div`
+    margin-top: 30px;
+`;
+
+export const Section = styled.div`
+    margin-bottom: 20px;
+`;
+
+export const SectionSex = styled.div`
+    display: flex;
+    gap:10px;
+    margin-bottom: 20px;
+`;
+
+export const SectionIam = styled.div`
+    
+`;
+
+
+export const Select = styled.select`
+    border: 1px solid ${({theme}) => theme.Colors.vinho};
+    width: 25%;
+`;
--- src/pages/formulario/firstStap/firstStap.tsx
+++ src/pages/formulario/firstStap/firstStap.tsx
@@ -1,18 +1,73 @@
-import Button from "../../../components/button/button"
-import { Text } from "../../../components/text/text"
-import { Container } from "../formulario.styles"
-
+import { useState } from "react";
+import Select from "react-select";
+import { Input } from "../../../components/input/input";
+import { Text } from "../../../components/text/text";
+import { useAuth } from "../../../hooks/useAuth";
+import InputCheck from "../components/InputCheck";
+import { Container } from "../formulario.styles";
+import { Section, SectionGlobalInputs, SectionIam, SectionSex, TitleStap } from "./firsStap.styles";
 
 export const FirstStap = () => {
+  const { user } = useAuth();
+
+  const [desire, setDesire] = useState("");
+  const [sex, setSex] = useState("");
+  const [height, setHeight] = useState("");
+  const [age, setAge] = useState("");
+  const [weight, setWeight] = useState("");
+
+  const options = [
+    { value: "Sedentarismo", label: "Sedentarismo" },
+    { value: "Atividade física moderada", label: "Atividade física moderada" },
+    { value: "Atividade física intensiva", label: "Atividade física intensiva" },
+  ]
+
   return (
     <Container>
-        <Text weight={400} height={16} size="14" color="vinho">Etapa 1</Text>
-        <Button variant="primario" title="Reconhecimento Físico"/>
-        <Text weight={400} height={21} size="18" color="preto">Eu desejo:</Text>
+      <TitleStap>
+        <Text weight={600} height={16} size="20" color="vinho">Etapa 1</Text>
+      </TitleStap>
 
-        <Text weight={400} height={21} size="18" color="preto">Eu sou:</Text>
-        <Button variant="primario" title="Anterior"/>
-        <Button variant="primario" title="Próximo"/>
+      <TitleStap>
+        <Text weight={600} height={21} size="20" color="vinho">Reconhecimento físico</Text>
+      </TitleStap>
+
+      <SectionGlobalInputs>
+        <Section>
+          <Text weight={600} height={20} size="18" color="preto">Eu desejo:</Text>
+          <InputCheck label="Perder peso" onChange={(e) => setDesire(e)} value={desire} type="checkbox" />
+          <InputCheck label="Manter meu peso atual" onChange={(e) => setDesire(e)} value={desire} type="checkbox" />
+          <InputCheck label="Ganhar massa muscular" onChange={(e) => setDesire(e)} value={desire} type="checkbox" />
+        </Section>
+
+        <Text weight={600} height={20} size="18" color="preto">Eu sou:</Text>
+        <SectionSex>
+          <SectionIam>
+            <InputCheck label="Feminino" onChange={(e) => setSex(e)} value={sex} type="checkbox" />
+            <InputCheck label="Masculino" onChange={(e) => setSex(e)} value={sex} type="checkbox" />
+            <InputCheck label="Não binário" onChange={(e) => setSex(e)} value={sex} type="checkbox" />
+            <InputCheck label="Homem Trans" onChange={(e) => setSex(e)} value={sex} type="checkbox" />
+
+          </SectionIam>
+          <SectionIam>
+            <InputCheck label="Mulher Trans" onChange={(e) => setSex(e)} value={sex} type="checkbox" />
+            <InputCheck label="Fluido" onChange={(e) => setSex(e)} value={sex} type="checkbox" />
+            <InputCheck label="Outro" onChange={(e) => setSex(e)} value={sex} type="checkbox" />
+            <InputCheck label="Prefiro não responder" onChange={(e) => setSex(e)} value={sex} type="checkbox" />
+          </SectionIam>
+        </SectionSex>
+
+        <Section>
+          <Input label="Sua altura:" onChange={(e) => setHeight(e)} value={height} />
+          <Input label="Sua idade:" onChange={(e) => setAge(e)} value={age} />
+          <Input label="Seu peso:" onChange={(e) => setWeight(e)} value={weight} />
+        </Section>
+
+
+        <Text weight={600} height={22} size="20" color="vinho">Nível de atividade física</Text>
+        <Select options={options} />
+      </SectionGlobalInputs>
+
     </Container>
   )
 }
