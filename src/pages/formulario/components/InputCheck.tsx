65f5f5aa5ac743a551305acefa99ef7ae03b66b1
Claudia dos Santos Lopes <lopes.claudia.csl@gmail.com>
2023-06-07 11:40:33 -0300

    feat: created component inputCheck

---
+++ src/pages/formulario/components/InputCheck.tsx
@@ -0,0 +1,41 @@
+import { ChangeEvent } from "react";
+import { Text } from "../../../components/text/text";
+import { InputWrapper, StyledInput } from "./InputCheck.styles";
+
+interface IInputCheck {
+  label: string;
+  value: string;
+  placeholder?: string;
+  onChange: (value: string) => void;
+  type?: string;
+  height?: string;
+}
+
+const InputCheck = ({
+  label,
+  value,
+  placeholder,
+  type = "text",
+  onChange,
+  height,
+}: IInputCheck) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.value);
+  };
+  return (
+    <InputWrapper>
+      <StyledInput
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        onChange={handleChange}
+        height={height}
+      />
+      <Text color="preto" size="18" weight={400} height={22}>
+        {label}
+      </Text>
+    </InputWrapper>
+  )
+}
+
+export default InputCheck
