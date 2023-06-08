d58a09f4e888f7a09bab56e6bcb9e8f8fd03c1d6
Samuel Allan Nunes da Cunha <117799926+SAMUKISZHSD@users.noreply.github.com>
2023-06-08 17:34:28 -0300

    add responsividade

--- src/pages/signInUser/signInUser.styles.ts
+++ src/pages/signInUser/signInUser.styles.ts
@@ -1,12 +1,11 @@
 import styled from "styled-components";
+import media from "styled-media-query";
 
 export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-
   flex-direction: column;
-
   height: 100%;
 `;
 
@@ -14,20 +13,30 @@ export const Header = styled.div``;
 
 export const Form = styled.div`
   margin-top: 36px;
-
   display: flex;
   align-items: center;
   justify-content: center;
-
   flex-direction: column;
-
   gap: 24px;
+
+  ${media.lessThan("medium")`
+    width: 100%;
+    padding: 0 16px;
+  `}
 `;
 
 export const InputWrapper = styled.div`
   width: 600px;
+
+  ${media.lessThan("medium")`
+    width: 100%;
+  `}
 `;
 
 export const ButtonWrapper = styled.div`
   width: 200px;
+
+  ${media.lessThan("medium")`
+    width: 100%;
+  `}
 `;
