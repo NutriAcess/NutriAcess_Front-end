e616d0caca1ddbb839d162a19ad45dea83e8437d
Samuel Allan Nunes da Cunha <117799926+SAMUKISZHSD@users.noreply.github.com>
2023-06-08 16:40:58 -0300

    add styled media query

--- src/agendamentos/agendamentos.styles.ts
+++ src/agendamentos/agendamentos.styles.ts
@@ -1,23 +1,27 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import media from "styled-media-query";
 
 export const Container = styled.div`
-
-
+  
 `;
-export const Content = styled.div `
+
+export const Content = styled.div`
   display: flex;
-
   flex-direction: column;
-
   align-items: center;
   justify-content: center;
-
-  background-color: ${({theme}) => theme.Colors.amarelo};
-
+  background-color: ${({ theme }) => theme.Colors.amarelo};
   height: 22rem;
+
+  ${media.lessThan("medium")`
+    width: 420px;
+    height: auto;
+    padding: 2rem;
+  `}
 `;
 
 export const Title = styled.span`
+  
 `;
 
 export const Description = styled.p`
@@ -25,7 +29,13 @@ export const Description = styled.p`
 `;
 
 export const ContentButton = styled.div`
-margin-top: 4rem;
+  margin-top: 4rem;
+
+  ${media.lessThan("medium")`
+    
+    margin-top: 2rem;
+  `}
 `;
+
 
 
