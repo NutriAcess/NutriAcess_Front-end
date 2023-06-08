ca4674787be069ea11afafae07ebb2f233c127c0
Luis Santana <luisroyale211@gmail.com>
2023-06-08 15:27:40 -0300

    fix: create divcircle and divstep style

--- src/pages/formulario/formulario.styles.ts
+++ src/pages/formulario/formulario.styles.ts
@@ -9,7 +9,19 @@ export const Container = styled.div`
 export const Content = styled.div`
     display: flex;
     margin-top: 50px;
-    margin-bottom: 20px;
+    justify-content: flex-end;
+    width: 300px;
+`;
+
+export const DivCircle = styled.button`
+    display: flex;
+    background: none;
+`;
+export const DivStep = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin-left: 30px;
 `;
 
 export const Box = styled.div`
