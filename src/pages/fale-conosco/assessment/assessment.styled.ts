7adbe30a37a2bbd74a426448759b9a505767784f
Samuel Allan Nunes da Cunha <117799926+SAMUKISZHSD@users.noreply.github.com>
2023-06-08 18:16:25 -0300

    feat: add reponsive

--- src/pages/fale-conosco/assessment/assessment.styled.ts
+++ src/pages/fale-conosco/assessment/assessment.styled.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import media from "styled-media-query";
 
 export const Container = styled.div`
   width: 100%;
@@ -9,6 +10,11 @@ export const Title = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  ${media.lessThan("medium")`
+    font-size: 1.5rem;
+    margin-top: 30px;
+  `}
 `;
 
 export const Feedback = styled.div`
@@ -17,6 +23,10 @@ export const Feedback = styled.div`
   justify-content: center;
   margin-top: 45px;
   cursor: pointer;
+
+  ${media.lessThan("medium")`
+    margin-top: 20px;
+  `}
 `;
 
 export const DivNumber = styled.div`
@@ -28,11 +38,20 @@ export const DivNumber = styled.div`
   border-radius: 10px;
   margin-right: 3px;
   background-color: ${({ theme }) => theme.Colors.vinho};
+
+  ${media.lessThan("medium")`
+    height: 30px;
+    width: 30px;
+  `}
 `;
 
 export const DivInput = styled.div`
   margin-top: 40px;
   width: 35%;
+
+  ${media.lessThan("medium")`
+    width: 100%;
+  `}
 `;
 
 export const ContainerInput = styled.div`
@@ -48,6 +67,10 @@ export const ButtonDiv = styled.div`
   margin-bottom: 40px;
   width: 15%;
   margin-top: 34px;
+
+  ${media.lessThan("medium")`
+    width: 30%;
+  `}
 `;
 
 export const Number = styled.div`
@@ -56,4 +79,8 @@ export const Number = styled.div`
 
 export const Input = styled.div`
   color: ${({ theme }) => theme.Colors.branco};
+
+  ${media.lessThan("medium")`
+    font-size: 0.8rem;
+  `}
 `;
--- src/pages/fale-conosco/service/service.styles.ts
+++ src/pages/fale-conosco/service/service.styles.ts
@@ -1,63 +1,80 @@
 import styled from "styled-components";
+import media from "styled-media-query";
 
 export const Container = styled.div`
-width: 100%;
+  width: 100%;
 `;
 
 export const Title = styled.div`
-margin-top: 60px;
-margin-bottom: 40px;
-padding-left: 150px;
+  margin-top: 60px;
+  margin-bottom: 40px;
+  padding-left: 150px;
+
+  ${media.lessThan("medium")`
+    padding-left: 50px;
+   font-size: 250%;
+  `}
 `;
 
 export const DivGeneral = styled.div`
-display:flex;
-align-items:center;
-justify-content:space-evenly;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+
+  ${media.lessThan("medium")`
+    flex-direction: column;
+    align-items: stretch;
+  `}
 `;
 
 export const DivNumber = styled.div`
-width: 310px;
-height: 300px;
-display:flex;
-flex-direction:column;
-border-radius: 20px;
-align-items:center;
-box-shadow: 0 3px 3px rgba(0, 0, 0, 0.25);
+  width: 310px;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 20px;
+  align-items: center;
+  box-shadow: 0 3px 3px rgba(0, 0, 0, 0.25);
+
+  ${media.lessThan("medium")`
+    width: 100%;
+    margin-bottom: 20px;
+  `}
 `;
 
 export const POne = styled.p`
-margin-top: 20px;
-font-size: 18px;
+  margin-top: 20px;
+  font-size: 18px;
 `;
 
 export const TelephoneLink = styled.a`
-margin-top: 60px;
-font-size: 14px;
-color:${({theme}) => theme.Colors.vinho};
+  margin-top: 60px;
+  font-size: 14px;
+  color: ${({ theme }) => theme.Colors.vinho};
+
+  ${media.lessThan("medium")`
+    margin-top: 30px;
+  `}
 `;
 
 export const TelephoneLink1 = styled.a`
-margin-top: 40px;
-font-size: 14px;
-color:${({theme}) => theme.Colors.vinho};
+  margin-top: 40px;
+  font-size: 14px;
+  color: ${({ theme }) => theme.Colors.vinho};
 `;
 
 export const DivRedes = styled.div`
- display: inline-block;
+  display: inline-block;
 `;
 
 export const DivTeste = styled.div`
- display: flex;
- flex-direction: column;
- margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
 `;
 
 export const ImgTel = styled.img`
-margin-top: 30px;
+  margin-top: 30px;
 `;
 
-export const ImgRedes = styled.img`
-
-`;
-
+export const ImgRedes = styled.img``;
