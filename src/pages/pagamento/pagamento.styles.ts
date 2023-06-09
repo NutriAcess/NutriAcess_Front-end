fe79a39e566e97d741bb86fd8cb534eeddc91be6
georgebias <georgeluancontato@gmail.com>
2023-06-09 00:09:05 -0300

    styles: subindo estilos

---
+++ src/pages/pagamento/pagamento.styles.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+`;
+
+export const Content = styled.div`
+  display: flex;
+  margin-top: 100px;
+`;
+
+export const SideRight = styled.div`
+  width:50%;
+  margin-left: 50px;
+`;
+
+export const HeaderPayment = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const ButtonConfirm = styled.div`
+    display: flex;
+    margin: 10px 30px;
+    justify-content: center;
+    align-items: center;
+`;
