70c06449da9867cbaa49a555eb3ea8346653da12
Luis Santana <luisroyale211@gmail.com>
2023-06-19 18:31:37 -0300

    feat: create popoupRegister

---
+++ src/components/popupcadastro/popup.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Container, DivImageSpecialist, DivImageUser, DivModalSignUp, DivX, DivsText, Header, InsideDivSpecialist, InsideDivUser, Question } from './popup.styles'
+import { Text } from '../text/text';
+import { Stethoscope, X } from '@phosphor-icons/react';
+import { User } from 'phosphor-react';
+import { useNavigate } from 'react-router-dom';
+
+interface ModalProps {
+  open: boolean;
+  onClose: () => void;
+}
+export interface IPopUp {
+  open: boolean;
+}
+
+export const PopUp = ({open, onClose}: ModalProps) => {
+  const navigate = useNavigate();
+
+  if(!open) return null;
+  return (
+    <Container open={open}>
+
+
+      <DivModalSignUp>
+        <Header>
+          <Question>
+            <Text color='preto' height={21} size="24" weight={700}>Como você quer se cadastrar?</Text>
+          </Question>
+          <DivX>
+            <X size={32} color='#000000' onClick={onClose} />
+          </DivX>
+        </Header>
+
+        <InsideDivUser onClick={() => navigate("/sign-up-user")}>
+          <DivImageUser>
+            <User size={48} color='#fff' />
+            <DivsText>
+              <Text color='branco' height={21} size="20" weight={700}>Cliente</Text>
+              <Text color='branco' height={21} size="14" weight={500}>Como você quer se cadastrar?</Text>
+            </DivsText>
+          </DivImageUser>
+        </InsideDivUser>
+
+        <InsideDivSpecialist onClick={() => navigate("/sign-up-esp")}>
+          <DivImageSpecialist>
+            <Stethoscope size={48} color='#fff' />
+            <DivsText>
+              <Text color='branco' height={21} size="20" weight={700}>Nutricionista</Text>
+              <Text color='branco' height={21} size="14" weight={500}>Quero atender pacientes de forma online, investir na minha carreira e com horários flexíveis!</Text>
+            </DivsText>
+          </DivImageSpecialist>
+        </InsideDivSpecialist>
+      </DivModalSignUp>
+
+
+    </Container>
+  )
+}
+
+
