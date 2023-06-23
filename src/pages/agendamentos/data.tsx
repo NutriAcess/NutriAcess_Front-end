a36e98fc8482d5d4efb772740f19432444cd8d23
Claudia dos Santos Lopes <lopes.claudia.csl@gmail.com>
2023-06-23 16:22:40 -0300

    feat: add slug and specialty agendamentos

--- src/pages/agendamentos/data.tsx
+++ src/pages/agendamentos/data.tsx
@@ -10,92 +10,78 @@ import LogoVerify from "../../assets/verify.svg";
 export const data = [
   {
     title: "Camila Gabriela",
-
+    slug: "camila-gabriela",
     verify: { alt: "", url: LogoVerify },
-
     socialmedia: "camig-nutri",
-
+    specialty: "Nutrição Esportiva",
     description:
       "Olá! Me chamo Camila, sou nutricionista e atuo com demandas de compulsividade, veganismo, vida física ativa, flexibilidade, entre outros. Posso te ajudar nessa caminhada rumo ao autoconhecimento e equilíbrio. Conte comigo, agende sua consulta.",
-
     image: { alt: "", url: ftprofissional },
   },
 
   {
     title: "Rogério Motta",
-
+    slug: "rogerio-motta",
     verify: { alt: "", url: LogoVerify },
-
     socialmedia: "motta-nutri",
-
+    specialty: "Nutrição Estética",
     description:
       "Olá! Sou um profissional dedicado e apaixonado por ajudar as pessoas a alcançarem uma alimentação saudável. Tenho vasto conhecimento em orientação personalizada, visando o bem-estar e a qualidade de vida dos meus pacientes.",
-
     image: { alt: "", url: ftprofissional2 },
   },
 
   {
     title: "Luciano Barbosa",
-
+    slug: "luciano-barbosa",
     verify: { alt: "", url: LogoVerify },
-
     socialmedia: "luciano-barbosa-nutri",
-
+    specialty: "Nutrição Funcional",
     description:
       "Seja bem vindo(a)!   Sou nutricionista especialista em nutrição esportiva e familiar. Posso te ajudar a viver bem e conquistar mais qualidade de vida. Ficou curioso? Então me siga e marque uma consulta e inicie agora o seu processo de autoconhecimento!",
-
     image: { alt: "", url: ftprofissional3 },
   },
 
   {
     title: "Luiza Silva",
-
+    slug: "luiza-silva",
     verify: { alt: "", url: "" },
-
     socialmedia: "lulu_silva",
-
+    specialty: "Nutrição Materno-Infantil",
     description:
       "Uma nutricionista apaixonada pela conexão entre alimentação e bem-estar. Com meu conhecimento em nutrição funcional, posso te ajudar a alcançar equilíbrio, energia e uma relação saudável com a comida. Vamos embarcar juntos nessa jornada rumo a uma vida mais saudável e feliz? ",
-
     image: { alt: "", url: ftprofissional4 },
   },
 
   {
     title: "Anthony Barsalobre",
-
+    slug: "anthony-barsalobre",
     verify: { alt: "", url: LogoVerify },
-
     socialmedia: "barsalobre_",
-
+    specialty: "Nutrição Integrativa",
     description:
       "Nutricionista dedicado a promover uma alimentação consciente e prazerosa. Acredito que cada pessoa é única, e meu objetivo é auxiliar você a encontrar o equilíbrio nutricional ideal para o seu corpo e estilo de vida. Juntos, vamos desvendar os segredos de uma alimentação saudável, saborosa e sustentável!",
-
     image: { alt: "", url: ftprofissional5 },
   },
 
   {
     title: "Maria Silveira",
-
+    slug: "maria-silveira",
     verify: { alt: "", url: "" },
-
     socialmedia: "Mariaa-nutrition",
-
+    specialty: "Nutrição Familiar",
     description:
       "Nutricionista apaixonada por desvendar o poder dos alimentos. Minha missão é te ajudar a alcançar uma saúde vibrante e duradoura através da alimentação. Com abordagem personalizada e baseada em evidências científicas, vamos construir uma relação positiva com a comida e conquistar seus objetivos de forma equilibrada!",
-
     image: { alt: "", url: ftprofissional6 },
   },
 
   {
     title: "Paula Lima",
-
+    slug: "paula-lima",
     verify: { alt: "", url: LogoVerify },
-
     socialmedia: "paula_nutri",
-
+    specialty: "Nutrição Esportiva",
     description:
       "A nutricionista que vai te mostrar que comer bem não precisa ser chato! Vamos dar um up na sua alimentação e turbinar sua energia? Com dicas práticas, receitas deliciosas e um toque de bom humor, vamos desbravar o mundo da nutrição juntos. Chega de dietas mirabolantes, vamos construir um estilo de vida saudável e cheio de sabor!",
-
     image: { alt: "", url: ftprofissional7 },
   },
 ];
